a8b3b04c36fff30f93fc1130db2a199d1b1582d5
Vincent Dubuc <cerise.litchis.0z@icloud.com>
2024-11-23 20:46:18 +0100

    Ajout de donnée plus coherente autour de la ville de sussargues

--- src/main/java/polytech/service_emplacements/service/EmplacementService.java
+++ src/main/java/polytech/service_emplacements/service/EmplacementService.java
@@ -18,14 +18,93 @@ public class EmplacementService {
         this.emplacementRepository = emplacementRepository;
     }
 
-    public void PopulateDb () {
+    public void PopulateDb() {
         emplacementRepository.deleteAll();
-        Emplacement emplacement1 = new Emplacement(1L, "Emplacement 1", "Adresse 1", "Description 1", List.of("Commodité 1", "Commodité 2"), null, 1.0, 1.0, 1.0, "2021-01-01", "2021-01-02");
-        Emplacement emplacement2 = new Emplacement(2L, "Emplacement 2", "Adresse 2", "Description 2", List.of("Commodité 3", "Commodité 4"), null, 2.0, 2.0, 2.0, "2021-01-03", "2021-01-04");
-        Emplacement emplacement3 = new Emplacement(3L, "Emplacement 3", "Adresse 3", "Description 3", List.of("Commodité 5", "Commodité 6"), null, 3.0, 3.0, 3.0, "2021-01-05", "2021-01-06");
-        Emplacement emplacement4 = new Emplacement(1L, "Emplacement 4", "Adresse 1", "Description 1", List.of("Commodité 1", "Commodité 2"), null, 1.0, 1.0, 1.0, "2021-01-01", "2021-01-02");
-        Emplacement emplacement5 = new Emplacement(2L, "Emplacement 5", "Adresse 2", "Description 2", List.of("Commodité 3", "Commodité 4"), null, 2.0, 2.0, 2.0, "2021-01-03", "2021-01-04");
-        Emplacement emplacement6 = new Emplacement(3L, "Emplacement 6", "Adresse 3", "Description 3", List.of("Commodité 5", "Commodité 6"), null, 3.0, 3.0, 3.0, "2021-01-05", "2021-01-06");
+
+        Emplacement emplacement1 = new Emplacement(
+                1L,
+                "Camping Les Oliviers",
+                "Chemin des Oliviers, Sussargues",
+                "Un charmant emplacement entouré d'oliviers.",
+                List.of("Eau potable", "Électricité"),
+                null,
+                43.714158,
+                4.003973,
+                25.0,
+                "2024-01-01",
+                "2024-12-31"
+        );
+
+        Emplacement emplacement2 = new Emplacement(
+                2L,
+                "Bivouac La Garrigue",
+                "Route de la Garrigue, Sussargues",
+                "Idéal pour les amateurs de nature sauvage.",
+                List.of("Toilettes sèches", "Barbecue"),
+                null,
+                43.712158,
+                4.005973,
+                30.0,
+                "2024-02-01",
+                "2024-11-30"
+        );
+
+        Emplacement emplacement3 = new Emplacement(
+                3L,
+                "Aire de Camping Les Vignes",
+                "Rue des Vignes, Sussargues",
+                "Proche des vignobles, parfait pour une escapade tranquille.",
+                List.of("Wi-Fi", "Douche chaude"),
+                null,
+                43.711158,
+                4.002973,
+                35.0,
+                "2024-03-01",
+                "2024-10-31"
+        );
+
+        Emplacement emplacement4 = new Emplacement(
+                4L,
+                "Espace Nature",
+                "Avenue de la Nature, Sussargues",
+                "Un emplacement spacieux avec une vue dégagée.",
+                List.of("Table de pique-nique", "Point d'eau"),
+                null,
+                43.715158,
+                4.004973,
+                20.0,
+                "2024-04-01",
+                "2024-09-30"
+        );
+
+        Emplacement emplacement5 = new Emplacement(
+                5L,
+                "Aire de Repos Les Pins",
+                "Impasse des Pins, Sussargues",
+                "Idéal pour se reposer sous les pins ombragés.",
+                List.of("Eau potable", "Parking gratuit"),
+                null,
+                43.713658,
+                4.007973,
+                15.0,
+                "2024-05-01",
+                "2024-08-31"
+        );
+
+        Emplacement emplacement6 = new Emplacement(
+                6L,
+                "Bivouac du Lac",
+                "Chemin du Lac, Sussargues",
+                "Proche d'un petit lac pour une expérience unique.",
+                List.of("Pêche autorisée", "Barbecue"),
+                null,
+                43.710158,
+                4.001973,
+                40.0,
+                "2024-06-01",
+                "2024-07-31"
+        );
+
         emplacementRepository.save(emplacement1);
         emplacementRepository.save(emplacement2);
         emplacementRepository.save(emplacement3);
